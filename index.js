1e0de3ee167c43b32074d9bfb6eb3f859e027379
Pedro Henrique Novelli Soares <125471109+Novellii@users.noreply.github.com>
2023-08-30 21:22:48 -0300

    Novas Opções
    Agora os jogadores poderão escolher o número máximo do dado, 1d4, 1d6, 1d8, 1d10, 1d12, 1d20, 1d50, 1d100.

--- index.js
+++ index.js
@@ -1,32 +1,55 @@
-document.getElementById("TextoResultado").innerHTML = Math.floor(Math.random() * 20) + 1;
+$(document).ready(function () {
+    $("#rolar").click(function () {
+        var opcao = $('#dado').val();
+        var resultado = 0;
 
-var speed = 20;
+        var speed = 20;
 
-function incEltNbr(id) {
-    elt = document.getElementById(id);
-    endNbr = Number(document.getElementById(id).innerHTML);
-    incNbrRec(0, endNbr, elt);
-}
+        function incEltNbr(id) {
+            var elt = document.getElementById(id);
+            var endNbr = Number(document.getElementById(id).innerHTML);
+            incNbrRec(0, endNbr, elt);
+        }
 
-function incNbrRec(i, endNbr, elt) {
-    if (i <= endNbr) {
-        elt.innerHTML = i;
-        setTimeout(function () {
-            incNbrRec(i + 1, endNbr, elt);
-        }, speed);
-    }
-}
+        function incNbrRec(i, endNbr, elt) {
+            if (i <= endNbr) {
+                elt.innerHTML = i;
+                setTimeout(function () {
+                    incNbrRec(i + 1, endNbr, elt);
+                }, speed);
+            }
+        }
 
-function incNbr() {
-    incEltNbr("TextoResultado");
-}
 
-incEltNbr("TextoResultado");
+        if (opcao == 'option01') {
+            resultado = Math.floor(Math.random() * 100) + 1;
+        }
+        else if (opcao == 'option02') {
+            resultado = Math.floor(Math.random() * 50) + 1;
+        }
+        else if (opcao == 'option03') {
+            resultado = Math.floor(Math.random() * 20) + 1;
+        }
+        else if (opcao == 'option04') {
+            resultado = Math.floor(Math.random() * 12) + 1;
+        }
+        else if (opcao == 'option05') {
+            resultado = Math.floor(Math.random() * 10) + 1;
+        }
+        else if (opcao == 'option06') {
+            resultado = Math.floor(Math.random() * 8) + 1;
+        }
+        else if (opcao == 'option07') {
+            resultado = Math.floor(Math.random() * 6) + 1;
+        }
+        else {
+            resultado = Math.floor(Math.random() * 4) + 1;
+        }
+        $("#TextoResultado").text(resultado);
+        function incNbr() {
+            incEltNbr("TextoResultado");
+        }
 
-function roll20() {
-    return Math.random() * 21 | 1;
-}
-
-function resultado20() {
-    javascript: window.top.location.reload(true)
-}
+        incEltNbr("TextoResultado");
+    });
+});
